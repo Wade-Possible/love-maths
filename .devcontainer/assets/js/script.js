759cd90b943ea47b70fe15942fc8ce8a9da40fa4
JMB <137451269+Wade-Possible@users.noreply.github.com>
2024-02-25 12:56:46 +0100

    added the main functions of the game

--- .devcontainer/assets/js/script.js
+++ .devcontainer/assets/js/script.js
@@ -1,1 +1,24 @@
-console.log('Hello Wrold');
+function runGame() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function calculateCorrectAnswer() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrongAnswer() {
+
+}
+
+function displayAdditionQuestion() {}
+function displaySubtractionQuestion() {}
+function displayMultiplyQuestion() {}
+function displayDivisionQuestion() {}
