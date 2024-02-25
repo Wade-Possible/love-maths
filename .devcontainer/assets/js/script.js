ee340e7d6fd4fcdc2d1721c7a1d706c4c83a132d
JMB <137451269+Wade-Possible@users.noreply.github.com>
2024-02-25 14:43:50 +0100

    added evenlistener for the game

--- .devcontainer/assets/js/script.js
+++ .devcontainer/assets/js/script.js
@@ -1,3 +1,21 @@
+//get buttons and adding eventListeners for the game.
+
+document.addEventListener('DOMContentLoaded', function() {
+    let buttons = document.getElementsByTagName('button')
+
+    for (let button of buttons) {
+        //Try 'this instead of button!!!!
+        button.addEventListener('click', function() {
+           if (this.getAttribute('data-type') === 'submit') {
+               alert('You clicked submit') 
+           } else {
+            let gameType = this.getAttribute('data-type');
+            alert(`You clicked ${gameType}`)
+           }
+        })
+    }
+})
+
 function runGame() {
 
 }
