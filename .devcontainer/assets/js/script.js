4dfd91e695a2436e0d58886ec0782ad72e59540d
JMB <137451269+Wade-Possible@users.noreply.github.com>
2024-02-26 19:29:08 +0100

    added code to calculate two random numbers for the game

--- .devcontainer/assets/js/script.js
+++ .devcontainer/assets/js/script.js
@@ -4,7 +4,6 @@ document.addEventListener('DOMContentLoaded', function() {
     let buttons = document.getElementsByTagName('button')
 
     for (let button of buttons) {
-        //Try 'this instead of button!!!!
         button.addEventListener('click', function() {
            if (this.getAttribute('data-type') === 'submit') {
                alert('You clicked submit') 
@@ -16,8 +15,13 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 })
 
+/** The main game 'loop' called when the script is first loaded
+ * and when the users answer has been processed
+ */
 function runGame() {
-
+    //random numbers for the math game
+    let num1 = Math.floor(Math.random() * 25) + 1;
+    let num2 = Math.floor(Math.random() * 25) + 1;
 }
 
 function checkAnswer() {
