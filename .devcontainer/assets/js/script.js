2d01bbfc44459a06fbcf0462017adf4f39c64e4b
JMB <137451269+Wade-Possible@users.noreply.github.com>
2024-02-29 11:03:27 +0100

    Display additional questions to user

--- .devcontainer/assets/js/script.js
+++ .devcontainer/assets/js/script.js
@@ -9,19 +9,30 @@ document.addEventListener('DOMContentLoaded', function() {
                alert('You clicked submit') 
            } else {
             let gameType = this.getAttribute('data-type');
-            alert(`You clicked ${gameType}`)
+            runGame(gameType);
            }
         })
     }
+
+
+    runGame('addition');
 })
 
 /** The main game 'loop' called when the script is first loaded
  * and when the users answer has been processed
  */
-function runGame() {
+function runGame(gameType) {
     //random numbers for the math game
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
+
+    if (gameType ==='addition') {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert(`Unknown game type ${gameType}`);
+        throw `Unknown game type ${gameType}. Aborting!`;
+    }
+
 }
 
 function checkAnswer() {
@@ -40,7 +51,23 @@ function incrementWrongAnswer() {
 
 }
 
-function displayAdditionQuestion() {}
-function displaySubtractionQuestion() {}
-function displayMultiplyQuestion() {}
-function displayDivisionQuestion() {}
+function displayAdditionQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = '+';
+}
+function displaySubtractionQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = '-';
+}
+function displayMultiplyQuestion(operand1,operand2) {
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = 'x';
+}
+function displayDivisionQuestion(operand1,operand2) {
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = '/';
+}
