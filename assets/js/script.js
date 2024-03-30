02ea0b727c836c0dee2f90eeb3e15ef558cc5b40
JMB <137451269+Wade-Possible@users.noreply.github.com>
2024-03-30 11:17:24 +0100

    Done

--- assets/js/script.js
+++ assets/js/script.js
@@ -58,7 +58,7 @@ function checkAnswer() {
     let isCorrect = userAnswer === calculatedAnswer[0];
 
     if (isCorrect) {
-        alert('Hey! You got it right! :D');
+        alert('Hey! You got it Right! :D');
         incrementScore();
     } else {
         alert(`Awww... you answerd ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`);
